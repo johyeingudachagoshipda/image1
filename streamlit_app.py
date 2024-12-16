bd8db6d9cd325843befe71a73a829a25b0335e68
johyeingudachagoshipda <20242523@sdhs.gwe.hs.kr>
2024-12-16 11:45:27 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ import numpy as np  # numpy를 import해야 함
 st.title("주택 가격 예측 서비스!")
 
 # GitHub Raw 파일 URL과 모델 유형
-GITHUB_RAW_URL = ""
+GITHUB_RAW_URL = "https://github.com/johyeingudachagoshipda/image1/raw/refs/heads/main/xgb_model.pkl"
 MODEL_TYPE = "XGBoost"  # "fastai", "scikit-learn Random Forest", or "XGBoost"
 CSV_FILE_URL = ""
 
